fa8e8f77d4f0aa3649150335877c271a042ce0fb
Sergio Chamorro <serchrod96@gmail.com>
2022-01-09 17:01:28 -0600

    adding movie-rating relationship support

--- app/main/api/views/Movie/serializer.py
+++ app/main/api/views/Movie/serializer.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from main.models import Movie
+from main.models import Movie , Rating
 
+class RatingModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ['comment','user','rating','id']
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    
+    rating= RatingModelSerializer(many=True,source='movie_data')
+    
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = ['title','release_date','plot','rating']
